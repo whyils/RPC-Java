f59d84b187f32a87ec767100e0dc1bdd8da0bd29
WYL <whyils@163.com>
2025-02-16 23:29:20 +0800

    版本二开发到MessageType

--- study_version2/src/main/java/part1/common/Message/RpcResponse.java
+++ study_version2/src/main/java/part1/common/Message/RpcResponse.java
@@ -16,6 +16,8 @@ public class RpcResponse implements Serializable {
 
     private int code;
     private String message;
+    //更新：加入传输数据的类型，以便在自定义序列化器中解析
+    private Class<?> dataType;
     private Object data;
 
     public static RpcResponse success(Object data) {
