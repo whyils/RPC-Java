65fef1759361433e5bace5df01caf36a18197a01
WYL <whyils@163.com>
2025-02-20 23:09:44 +0800

    版本三part1 开发到一致性哈希部分

---
+++ study_version3/src/main/java/part1/Client/serviceCenter/balance/impl/ConsistencyHashLoadBalance.java
@@ -0,0 +1,27 @@
+package part1.Client.serviceCenter.balance.impl;
+
+import part1.Client.serviceCenter.balance.LoadBalance;
+
+import java.util.List;
+
+public class ConsistencyHashLoadBalance implements LoadBalance {
+
+
+    @Override
+    public String balance(List<String> addressList) {
+
+
+
+        return "";
+    }
+
+    @Override
+    public void addNode(String node) {
+
+    }
+
+    @Override
+    public void delNode(String node) {
+
+    }
+}
